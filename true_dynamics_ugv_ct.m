07870c1dbab348384ee3edbda416fd54341aa5d0
StellaKombo <155917899+StellaKombo@users.noreply.github.com>
2024-07-14 15:47:56 -0700

    Add files via upload

--- true_dynamics_ugv_ct.m
+++ true_dynamics_ugv_ct.m
@@ -1,40 +1,23 @@
 function dx_dt = true_dynamics_ugv_ct(t, x, u)
     % continous-time update equation of the nominal dynamics in earth frame.
+    % For a double integrator xdot = [xdot;ydot;xddot;yddot]
+    x_1 = x(1);
+    y = x(2);
+    x1_dot = x(3);
+    y_dot = x(4);
     
-    % Extract the current heading angle from the state vector x
-    %theta_position = x(3,:);
-    % Since we are using a linear MPc we have to use the new state
-    % variables
-
-    % Extract the current state variables
-    x_val = x(1);
-    y_val = x(2);
-    xdot_val = x(3);
-    ydot_val = x(4);
-
-    % Extract linear and angular velocity from the control input u
-    v_val = u(1);
-    w_val = u(2);
-
-
-    % % Extract perturbations (process noise)
-    %w_y = perturbations(2);
-    % w_xdot = perturbations(3);
-    % w_ydot = perturbations(4);
-
-    % Initialize the rate of change of the state vector (dx/dt)
-    dx_dt = zeros(size(x));
-
-    % % Update the rate of change of the state vector based on kinematic equations
-    dx_dt(1,:) = xdot_val + randn*0.1;
-    %dx_dt(2,:) = ydot_val + w_y;
-    % dx_dt(3,:) = v_val + w_xdot;
-    % dx_dt(4,:) = w_val + w_ydot;
-
-    % Update the rate of change of the state vector based on kinematic equations
-    %dx_dt(1,:) = xdot_val;
-    dx_dt(2,:) = ydot_val;
-    dx_dt(3,:) = v_val;
-    dx_dt(4,:) = w_val;
+    % Control inputs
+    u_x = u(1);  % Acceleration in x direction
+    u_y = u(2);  % Acceleration in y direction
+    
+    % Dynamics equations for 2D double integrator
+    x_ddot = u_x;
+    y_ddot = u_y;
+    
+    % State derivatives
+    dx_dt(1,1) = x1_dot + 0.1 * randn; 
+    dx_dt(2,1) = y_dot; 
+    dx_dt(3,1) = x_ddot; 
+    dx_dt(4,1) = y_ddot;
 
 end
